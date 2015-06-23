90e891adcb6142e86198ecb72a06063458b13c62
eriksf <erik@jcvi.org>
2015-06-23 17:01:04 -0400

    added service to get images (plus extra data) by line to ease display

---
+++ services/images_data_by_line/main.py
@@ -0,0 +1,38 @@
+import json
+import services.common.tools as tools
+
+def search(args):
+    """
+    args contains a dict with one or key:values
+
+    line is a combination of plate, well, plant & dip number (mandatory)
+    """
+    line = args['line']
+
+    """
+    Make the request to the remote service
+    """
+    response = tools.do_request('ImageDataPerLineID.php', line=line)
+
+    """
+    Iterate through the results
+    Foreach record from the remote service, build the response json
+    Print this json to stdout followed by a record separator "---"
+    ADAMA takes care of serializing these results
+    """
+    for result in response['images']:
+
+        record = {
+                'line': line,
+                'class': 'line_property',
+                'source_text_description': 'Reporter_Image_data',
+                'image_record': {
+                        'image_id': result['image_id'],
+                        'po_codes': result['po_codes']
+                }
+            }
+        print json.dumps(record, indent=2)
+        print '---'
+
+def list(args):
+    raise Exception('not implemented yet')
